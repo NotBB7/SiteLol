71e601c48f7425c707ebab57140603c110f20e07
NotBB <notice76610@gmail.com>
2023-06-14 12:47:47 +0200

    Update data.php

--- models/data.php
+++ models/data.php
@@ -1,144 +1,67 @@
 <?php
 
-$data = [
+$champion = [
 	[
-		"name" => "Tyrone Baardwijk",
-		"phone" => "07 37 48 03 84",
-		"email" => "libero.lacus@hotmail.net",
-		"postalZip" => "84353",
-		"region" => "Franche-Comté"
+		"name" => "Aatrox",
+		"prix" => " 4800 EB",
+		"poste" => "TOP",
+		"description" => "Epee des Darkin",
+		"region" => "Runeterra"
 	],
 	[
-		"name" => "Emmanuel Fabre",
-		"phone" => "05 52 39 81 90",
-		"email" => "commodo.tincidunt@google.edu",
-		"postalZip" => "96191",
-		"region" => "Haute-Normandie"
+		"name" => "Yuumi",
+		"prix" => "450 EB",
+		"poste" => "SUPPORT",
+		"description" => "Gardienne du Grimoire",
+		"region" => "Bandle"
 	],
 	[
-		"name" => "Desiree Bakhuizen",
-		"phone" => "05 82 85 44 25",
-		"email" => "curabitur.dictum.phasellus@outlook.net",
-		"postalZip" => "21566",
-		"region" => "Poitou-Charentes"
+		"name" => "Samira",
+		"prix" => "3150 EB",
+		"poste" => "ADC",
+		"description" => "Rose du Désert",
+		"region" => "Noxus"
 	],
 	[
-		"name" => "Lawrence Aakster",
-		"phone" => "02 63 52 83 16",
-		"email" => "pellentesque.eget.dictum@aol.net",
-		"postalZip" => "58594",
-		"region" => "Centre"
+		"name" => "Nami",
+		"prix" => "3150 EB",
+		"poste" => "SUPPORT",
+		"description" => "Aquamancienne",
+		"region" => "Runeterra"
 	],
 	[
-		"name" => "Coby Tailler",
-		"phone" => "01 64 88 77 22",
-		"email" => "aenean@protonmail.couk",
-		"postalZip" => "53882",
-		"region" => "Alsace"
+		"name" => "Ahri",
+		"prix" => "3150 EB",
+		"poste" => "MID",
+		"description" => "Renard à Neuf Queues",
+		"region" => "Ionia"
 	],
 	[
-		"name" => "Lev Bezuindenhout",
-		"phone" => "09 73 17 51 85",
-		"email" => "eros@yahoo.ca",
-		"postalZip" => "33088",
-		"region" => "Lorraine"
+		"name" => "Garen",
+		"prix" => "450 EB",
+		"poste" => "TOP",
+		"description" => "Force de Demacia",
+		"region" => "Demacia"
 	],
 	[
-		"name" => "Illiana Prinsen",
-		"phone" => "06 18 50 51 31",
-		"email" => "est.nunc@protonmail.com",
-		"postalZip" => "92602",
-		"region" => "Champagne-Ardenne"
+		"name" => "Nocturne",
+		"prix" => "3150 EB",
+		"poste" => "JUNGLE",
+		"description" => "Eternel Cauchemar",
+		"region" => "Runeterra"
 	],
 	[
-		"name" => "Pandora Duval",
-		"phone" => "02 26 21 73 17",
-		"email" => "ac.arcu@google.ca",
-		"postalZip" => "60512",
-		"region" => "Bretagne"
+		"name" => "Veigar",
+		"prix" => "1350 EB",
+		"poste" => "MID",
+		"description" => "Seigneur des Maléfices",
+		"region" => "Bandle"
 	],
 	[
-		"name" => "Sybill Vincent",
-		"phone" => "09 56 03 31 13",
-		"email" => "dolor.sit.amet@icloud.ca",
-		"postalZip" => "77334",
-		"region" => "Auvergne"
-	],
-	[
-		"name" => "Keith Lachapelle",
-		"phone" => "04 94 42 34 56",
-		"email" => "lorem.ipsum.dolor@outlook.com",
-		"postalZip" => "48498",
-		"region" => "Alsace"
-	],
-	[
-		"name" => "Genevieve Elzinga",
-		"phone" => "07 66 23 35 50",
-		"email" => "vivamus.euismod@outlook.com",
-		"postalZip" => "61719",
-		"region" => "Franche-Comté"
-	],
-	[
-		"name" => "Orlando Romeijnders",
-		"phone" => "05 51 68 86 81",
-		"email" => "ipsum.nunc.id@protonmail.ca",
-		"postalZip" => "89757",
-		"region" => "Corse"
-	],
-	[
-		"name" => "Athena Spijker",
-		"phone" => "07 11 93 61 28",
-		"email" => "integer.sem@aol.couk",
-		"postalZip" => "07871",
-		"region" => "Limousin"
-	],
-	[
-		"name" => "Brianna Segal",
-		"phone" => "04 04 87 51 18",
-		"email" => "ac.metus.vitae@google.net",
-		"postalZip" => "28577",
-		"region" => "Champagne-Ardenne"
-	],
-	[
-		"name" => "Sage Cloutier",
-		"phone" => "09 28 26 68 66",
-		"email" => "semper.nam@icloud.net",
-		"postalZip" => "75632",
-		"region" => "Poitou-Charentes"
-	],
-	[
-		"name" => "Octavius Lavigne",
-		"phone" => "01 56 85 44 73",
-		"email" => "ullamcorper.nisl@hotmail.org",
-		"postalZip" => "44711",
-		"region" => "Île-de-France"
-	],
-	[
-		"name" => "Renee Berg",
-		"phone" => "06 51 74 77 86",
-		"email" => "accumsan.convallis@google.edu",
-		"postalZip" => "58485",
-		"region" => "Nord-Pas-de-Calais"
-	],
-	[
-		"name" => "Dillon Dubois",
-		"phone" => "02 80 78 73 41",
-		"email" => "risus.morbi@hotmail.net",
-		"postalZip" => "77479",
-		"region" => "Franche-Comté"
-	],
-	[
-		"name" => "Justina Bunschoten",
-		"phone" => "06 85 13 49 51",
-		"email" => "arcu.imperdiet@aol.com",
-		"postalZip" => "76452",
-		"region" => "Aquitaine"
-	],
-	[
-		"name" => "Iris Kuiper",
-		"phone" => "06 54 76 76 73",
-		"email" => "malesuada@aol.org",
-		"postalZip" => "05951",
-		"region" => "Champagne-Ardenne"
+		"name" => "Trundle",
+		"prix" => "3150 EB",
+		"poste" => "JUNGLE",
+		"description" => "Roi des trolls",
+		"region" => "Freijord"
 	]
 ];
