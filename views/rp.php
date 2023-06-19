a21a34343335b0aec4d2a809a0874d468852ae93
NotBB <notice76610@gmail.com>
2023-06-19 09:28:26 +0200

    modif

--- views/rp.php
+++ views/rp.php
@@ -4,27 +4,27 @@
 
 <?php $cardCount = 0; ?>
 <?php foreach ($rp as $info) { ?>
-    <?php if ($cardCount < 6) { ?>
+  <?php if ($cardCount < 6) { ?>
 
-        <section>
-        <div class="wrapper">
-    <div class="product-img">
-      <img src="https://assets.millionpugs.com/redeem/prd/RIOT_Points_100_RP_66de8be237.png" height="420" width="327">
-    </div>
-    <div class="product-info">
-      <div class="product-text">
-        <h1><?= $info['nombre']; ?></h1>
-        <h2><?= $info['bonus']; ?></h2>
-        <p><?= $info['description']; ?> </p>
+    <section>
+      <div class="wrapper">
+        <div class="product-img">
+          <img src="https://assets.millionpugs.com/redeem/prd/RIOT_Points_100_RP_66de8be237.png" height="420" width="327">
+        </div>
+        <div class="product-info">
+          <div class="product-text">
+            <h1><?= $info['nombre']; ?></h1>
+            <h2><?= $info['bonus']; ?></h2>
+            <p><?= $info['description']; ?> </p>
+          </div>
+          <div class="product-price-btn">
+            <p><?= $info['prix']; ?></p>
+            <button type="button">Achetez</button>
+          </div>
+        </div>
       </div>
-      <div class="product-price-btn">
-        <p><?= $info['prix']; ?></p>
-        <button type="button">Achetez</button>
-      </div>
-    </div>
-  </div>
-        </section>
-        <?php $cardCount++; ?>
-    <?php } ?>
+    </section>
+    <?php $cardCount++; ?>
+  <?php } ?>
 <?php } ?>
 <?php include "components/footer.php" ?>
